97479410d6cbca2eff408a8f7671a1a5bc82a11e
Chloe2330 <55333255+Chloe2330@users.noreply.github.com>
2023-11-13 03:21:23 -0500

    backend selection process + mutexes

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"sync/atomic"
 )
 
 type Backend struct {
@@ -15,5 +19,68 @@ type Backend struct {
 
 type ServerPool struct {
 	backends []*Backend
-	current  uint64 // counter variable
+	current  uint64
+}
+
+func (s *ServerPool) NextIndex() int {
+	nextIndex := atomic.AddUint64(&s.current, uint64(1))
+	return int(nextIndex % uint64(len(s.backends)))
+}
+
+func (s *ServerPool) GetNextBackend() *Backend {
+	next := s.NextIndex()
+	length := len(s.backends) + next
+
+	// cyclic traversal of backends slice
+	for i := next; i < length; i++ {
+		index := i % len(s.backends)
+		// mark working backend as current one
+		if s.backends[index].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(index))
+			}
+			return s.backends[index]
+		}
+	}
+	return nil
+}
+
+func (b *Backend) IsAlive() bool { // read operation
+	b.mux.RLock()
+	alive := b.Alive
+	b.mux.RUnlock()
+	return alive
+}
+
+func (b *Backend) SetAlive(alive bool) { // write operation
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+// load balancer logic (round-robin)
+func lb(w http.ResponseWriter, r *http.Request) {
+	peer := servers.GetNextBackend()
+	if peer != nil {
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
+var servers ServerPool
+
+func main() {
+	var port int
+
+	// create http server
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(lb),
+	}
+
+	log.Printf("Load Balancer started at :%d\n", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
